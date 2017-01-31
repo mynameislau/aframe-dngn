6d1c9fecbfa3fd08ea0b3739655d5203548a5071
DevLau <devlau@MacBook-Pro-de-Laurent.local>
2017-01-31 23:27:41 +0100

    functional lighting

--- src/js/components/wall.jsx
+++ src/js/components/wall.jsx
@@ -2,39 +2,45 @@ import React from 'react';
 import { Entity } from 'aframe-react';
 import { neighbours, orientation } from '../graph/grid';
 import R from 'ramda';
+import { Maybe } from 'ramda-fantasy';
+const { Just, Nothing } = Maybe;
 
-const Light = () =>
-  <Entity>
-      <Entity position="0 0 -1" light="color: #ff6300; decay: 2.38; distance: 13; intensity: 4.78; type: point;"/>
-      <Entity position="0 0 1" light="color: #ff6300; decay: 2.38; distance: 13; intensity: 4.78; type: point;"/>
-      <Entity position="1 0 0" light="color: #ff6300; decay: 2.38; distance: 13; intensity: 4.78; type: point;"/>
-      <Entity position="-1 0 0" light="color: #ff6300; decay: 2.38; distance: 13; intensity: 4.78; type: point;"/>
-      <Entity geometry="primitive: box; height: 0.1; width: 1.2; depth: 0.1" />
-      <Entity geometry="primitive: box; height: 0.1; width: 0.1; depth: 1.2" />
+const Light = vec =>
+  <Entity position={vec}>
+      <Entity light="color: #ff6300; decay: 2.38; distance: 13; intensity: 4.78; type: point;"/>
+      <Entity geometry="primitive: box; height: 0.1; width: 0.1; depth: 0.1" />
   </Entity>;
 
-const shouldPutLight = ({x, y}, board) => {
-  // console.log(neighbours(x, y, board));
-  //
-  // const getVec = orientation => {
-  //   switch (orientation) {
-  //     case 'N': return '0 0 -1'
-  //     case 'S': return '0 0 1'
-  //     case 'E': return '1 0 0'
-  //     case 'W': return '-1 0 0'
-  //   }
-  // }
-  //
-  // const adjFloor = R.compose(
-  //   getVec,
-  //   floorCell => orientation(x, y, floorCell.x, floorCell.y),
-  //   R.find(cell => cell.terrain === ' '),
-  //   neighbours(x, y, board)
-  // );
-  //
-  // console.log(adjFloor());
+const getLights = ({x, y}, board) => {
 
-  return Math.random() > 0.9;
+  const getVec = orientation => {
+    switch (orientation) {
+      case 'S': return '0 0 -1'
+      case 'N': return '0 0 1'
+      case 'E': return '1 0 0'
+      case 'W': return '-1 0 0'
+    }
+  }
+
+  const trace = val => {
+    console.log(val);
+    return val;
+  }
+
+  const adjFloorVec = R.compose(
+    R.map(getVec),
+    R.map(floorCell => orientation(x, y, floorCell.x, floorCell.y)),
+    R.filter(cell => cell && cell.terrain === ' '),
+    () => neighbours(x, y, board)
+  );
+
+  const setLights = R.compose(
+    R.map(adjFloorVec),
+    () => Math.random() > 0.9 ? Just() : Nothing([])
+  );
+  const toReturn = setLights().getOrElse([]);
+
+  return toReturn;
 }
 
 export default ({ cell, board }) => <Entity>
@@ -47,7 +53,7 @@ export default ({ cell, board }) => <Entity>
     geometry="primitive: box; height: 1; width: 1; depth: 1"
   ></Entity>
   {
-    shouldPutLight(cell, board) ? <Light /> : null
+    R.map(Light, getLights(cell, board))
   }
 </Entity>;
 
